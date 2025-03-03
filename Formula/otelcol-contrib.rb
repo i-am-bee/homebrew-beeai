5677c56dde8e4e221e8604e2cddb07bfa8408511
Jan Pokorný <JenomPokorny@gmail.com>
2025-03-03 17:30:51 +0000

    otelcol-contrib: add 0.120.0 bottle.

--- Formula/otelcol-contrib.rb
+++ Formula/otelcol-contrib.rb
@@ -5,6 +5,13 @@ class OtelcolContrib < Formula
   sha256 "2ccf43cc580d3b5bfad325204afccd966e92a9bcefef80fb75c08a8db567c496"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/i-am-bee/homebrew-beeai/releases/download/otelcol-contrib-0.120.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "581e8ffd10232f79f277b0cc3f800c1ffe3a12a3b3f0eef4b15344597427167b"
+    sha256 cellar: :any_skip_relocation, ventura:       "fb2a9093c72c2e8272ab092e1f49adeafacc0bb321c85cbd15a6341f9539d6f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec9d7046ae979f58163d3b53ffbf1b67c3d3548afb2baaed7dc57e483db46936"
+  end
+
   depends_on "go" => :build
 
   def install
