7c72145bbcf03619c1a8da5208c402e071920c9f
Jan Pokorný <JenomPokorny@gmail.com>
2025-05-22 15:43:51 +0000

    beeai: add 0.1.4 bottle.
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- Formula/beeai.rb
+++ Formula/beeai.rb
@@ -8,6 +8,14 @@ class Beeai < Formula
   license "Apache-2.0"
   head "https://github.com/i-am-bee/beeai.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/i-am-bee/homebrew-beeai/releases/download/beeai-0.1.4"
+    rebuild 1
+    sha256 arm64_sequoia: "27bf07110bccb32085cf23b94363b2d4eabc7a48903b7fa602a0efe7d9947579"
+    sha256 arm64_sonoma:  "ac0a30215728e00d1ad7f492a98fb3276e321b7833349a605bf7bc92acc2fe78"
+    sha256 ventura:       "7575f8a3385c0ad9814d19d295f7a47dec41c97ac474369ec128951f321dbfe2"
+  end
+
   depends_on "lima"
   depends_on "python@3.13"
 
