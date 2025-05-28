8244281c20cb9aa13e8168da66ac9855d6672bb1
Jan Pokorný <JenomPokorny@gmail.com>
2025-05-28 10:46:27 +0200

    bump: beeai 0.2.1
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- Formula/beeai.rb
+++ Formula/beeai.rb
@@ -3,17 +3,10 @@ class Beeai < Formula
 
   desc "Command-line interface and server for the BeeAI platform"
   homepage "https://github.com/i-am-bee/beeai"
-  url "https://files.pythonhosted.org/packages/df/3b/ba8b7ecdb706d077639755dff68369baa121332cc11ef84d5f8aea212bae/beeai_cli-0.2.0.tar.gz"
-  sha256 "1e9a9da49d998915b703c5fdaa2a8f710d5a37be4cd105694cdf6acd7548d22a"
+  url "https://files.pythonhosted.org/packages/00/44/5eb966ad88e461240e6f297c6627a3ad43ae339ad884f60bcf4b1fe0dbae/beeai_cli-0.2.1.tar.gz"
+  sha256 "bc596feb89206a99ddd54b06809c08019d90080a7a1d0510f6a0c2ac8d7f2beb"
   license "Apache-2.0"
   head "https://github.com/i-am-bee/beeai.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/i-am-bee/homebrew-beeai/releases/download/beeai-0.2.0"
-    sha256 arm64_sequoia: "1af0c7c4e99cab5e29609c050ce7aeae961c5920e060255dcfc5098df3fb58f1"
-    sha256 arm64_sonoma:  "f0403c3dfe07da6c9de38dc22ffa337ad83d2df41bf2946f68cb9d31b734cf96"
-    sha256 ventura:       "d936c838219b22db4826bd9d17aa981b94529554f54fcf873b190320f4eb5f27"
-  end
 
   depends_on "lima"
   depends_on "python@3.13"
