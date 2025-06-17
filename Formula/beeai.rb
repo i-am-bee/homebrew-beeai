1b805d13141fa20985db38f394b957597358fb92
Jan Pokorný <JenomPokorny@gmail.com>
2025-06-17 16:31:15 +0200

    chore(beeai): update to 0.2.9
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- Formula/beeai.rb
+++ Formula/beeai.rb
@@ -3,17 +3,10 @@ class Beeai < Formula
 
   desc "Command-line interface and server for the BeeAI platform"
   homepage "https://github.com/i-am-bee/beeai"
-  url "https://files.pythonhosted.org/packages/26/f6/08d383176fba4917a2feeed3600c052c99589198a1566c190fab3d6b2588/beeai_cli-0.2.7.tar.gz"
-  sha256 "d31ce9bb2ff691c3fe37c565f3a273536dac56f3187ba438975caaeaf0d791fa"
+  url "https://files.pythonhosted.org/packages/8e/5a/69e09e25d4108379b5ee8b495898099af2e26e7579a04b5f30e891b99549/beeai_cli-0.2.9.tar.gz"
+  sha256 "ee3348535bd1ed35d466a2f066b83f1c1b7eb9b1b61d56c16397895f4d2e6cb0"
   license "Apache-2.0"
   head "https://github.com/i-am-bee/beeai.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/i-am-bee/homebrew-beeai/releases/download/beeai-0.2.7"
-    sha256 cellar: :any, arm64_sequoia: "9dab37c6886ed08e26c02898b7a34d9e6dad644e3515b7763df841caa65c24b4"
-    sha256 cellar: :any, arm64_sonoma:  "1955a144be90b83646c3bebf7e6fb80a8540676f55fe244ead489d6874c95a1c"
-    sha256 cellar: :any, ventura:       "2662081a8a07b0c7b71c1e127b27c3dc44c195d3a808a7c31b440d3d82b5a850"
-  end
 
   depends_on "lima"
   depends_on "python@3.13"
